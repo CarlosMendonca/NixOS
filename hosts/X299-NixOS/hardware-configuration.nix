8b1bac90936eec9c8e51634a5245c915776af75e
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2023-08-09 01:20:28 -0400

    Adding a generated harware-configuration.nix; need to fix disk labels

--- hosts/X299-NixOS/hardware-configuration.nix
+++ hosts/X299-NixOS/hardware-configuration.nix
@@ -1,19 +1,38 @@
 { config, lib, pkgs, modulesPath, ... }: {
-    imports = [ ];
-
-    fileSystems."/" = {
-        device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
-        fsType = "ext4";
-    };
-
-    fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
-        fsType = "vfat";
-    };
-
-    swapDevices = [
-        { device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000"; }
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/ecdf62af-3ffb-41dd-a65d-3b52c22cd3aa";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/6832-331C";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/4b74ef0d-3182-40b1-9d06-632c2c0b269c"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
     hardware.opengl = {
         enable = true;
@@ -29,8 +48,6 @@
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
-
-    hardware.cpu.intel.updateMicrocode = true;
 
     nixpkgs.config.allowUnfree = true;
 }
