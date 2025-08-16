14c0073baf1ecb9a02be5f61af2eb5c5e74ba55a
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2025-08-16 08:28:04 -0400

    Moved nvidia bits inside a specialisation to make base configuration more power-efficient

--- hosts/X13-NixOS/hardware-configuration.nix
+++ hosts/X13-NixOS/hardware-configuration.nix
@@ -1,127 +1,139 @@
-{ config, ... }: {
-    imports = [ 
-      # ../../modules/bluetooth.nix # Bluetooth seems to work without needing to include this -- may be enabled elsewhere
-      # ../../modules/sound.nix # sounds like sound works without needing to enable this
+{ config, ... }:
+{
+  imports = [
+    # ../../modules/bluetooth.nix # Bluetooth seems to work without needing to include this -- may be enabled elsewhere
+    # ../../modules/sound.nix # sounds like sound works without needing to enable this
+  ];
+
+  boot = {
+    blacklistedKernelModules = [
+      "k10temp" # AMD temperature sensor?
+      "nouveau"
     ];
 
-    boot = {
-        blacklistedKernelModules = [
-            "k10temp" # AMD temperature sensor?
-            "nouveau"
-        ];
-        
-        extraModulePackages = [ config.boot.kernelPackages.zenpower ]; # TODO clarify what this is referring to
+    extraModulePackages = [ config.boot.kernelPackages.zenpower ]; # TODO clarify what this is referring to
 
-        initrd = {
-            availableKernelModules = [
-              "nvme"
-              "xhci_pci"
-              "thunderbolt"
-              "usbhid"
-              "usb_storage"
-              "sd_mod"
-              "sdhci_pci"
-            ];
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "thunderbolt"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+        "sdhci_pci"
+      ];
 
-            kernelModules = [ ];
-        };
-
-        kernelModules = [
-          "kvm-amd"
-          "zenpower"
-          "amd_pstate=active"
-          "wl" # wireless network
-        ];
-        
-        # kernelPackages = pkgs.linuxPackages_latest; # TODO define whether to use this or not
-        
-        kernelParams = [
-          "mem_sleep_default=deep"
-          "pcie_aspm.policy=powersupersave"          
-        ];
-         
-        loader = {
-            systemd-boot.enable = true;
-            systemd-boot.configurationLimit = 5;
-            efi.canTouchEfiVariables = true;
-        };
+      kernelModules = [ ];
     };
 
-    hardware = {
-        amdgpu.initrd.enable = true;
-
-        cpu.amd.updateMicrocode = true;
-
-        enableAllFirmware = true; # required for AX210, amdgpu, and potentially others
-
-        graphics = {
-            enable = true;
-            enable32Bit = true;
-        };
-
-        nvidia = {
-            dynamicBoost.enable = true;
-
-            modesetting.enable = true;
-            nvidiaSettings = true;
-            open = true;
-
-            powerManagement = {
-                enable = true;
-                finegrained = true;
-            };
-
-            prime = {
-                offload = {
-                    enable = true;
-                    enableOffloadCmd = true;
-                };
-            
-                amdgpuBusId = "PCI:69:0:0";
-                nvidiaBusId = "PCI:1:0:0";
-            };
-        };
-
-        sensor.iio.enable = true; # enable 2-in-1 sensors (orientation)
-    };
-
-    services = {
-        asusd = {
-            enable = true;
-            enableUserService = true;
-        }; # Asus control software
-
-        fstrim.enable = true;
-
-        supergfxd.enable = true; # Asus control software
-
-        tlp.enable = false; # TODO clarify what this is
-
-        udev = {
-            extraHwdb = ''
-                evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
-                KEYBOARD_KEY_ff31007c=f20
-            ''; # fixes mic mute button
-        };
-
-        # xserver.videoDrivers = ["nvidia"]; # TODO determine whether it needs to be specified and what value should be used
-    };
-
-    # Disable power auto-suspend for the ASUS N-KEY device, i.e. USB Keyboard
-    # Disable power wakeup for the 8295 ITE device
-    # Mediatek fine tuning -- not necessary since I'm running AX210
-
-    fileSystems."/" = {
-        device = "/dev/disk/by-label/nixos";
-        fsType = "ext4";
-    };
-
-    fileSystems."/boot" = {
-        device = "/dev/disk/by-label/SYSTEM"; # assume the Windows boot partition manually created with 512MB
-        fsType = "vfat";
-    };
-
-    swapDevices = [
-        { device = "/dev/disk/by-label/swap"; } # not sure if 32 or 64GB
+    kernelModules = [
+      "kvm-amd"
+      "zenpower"
+      "amd_pstate"
+      "wl" # wireless network
     ];
+
+    # kernelPackages = pkgs.linuxPackages_latest; # TODO define whether to use this or not
+
+    kernelParams = [
+      "mem_sleep_default=deep"
+      "pcie_aspm.policy=powersupersave"
+      "amdgpu.sg_display=0" # can help solve flickering/glitching display issues since Scatter/Gather code was reenabled
+      "amdgpu.dcdebugmask=0x10" # same as above
+
+      "initcall_blacklist=acpi_cpufreq_init"
+      "amd_pstate=passive"
+      "amd_pstate=active"
+    ];
+
+    loader = {
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 5;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  hardware = {
+    amdgpu.initrd.enable = true;
+
+    cpu.amd.updateMicrocode = true;
+
+    enableAllFirmware = true; # required for AX210, amdgpu, and potentially others
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    sensor.iio.enable = true; # enable 2-in-1 sensors (orientation)
+  };
+
+  services = {
+    asusd = {
+      enable = true;
+      enableUserService = true;
+    }; # Asus control software
+
+    fstrim.enable = true;
+
+    supergfxd.enable = true; # Asus control software
+
+    tlp.enable = false; # avoid conflicts with Gnome 40+; see https://github.com/NixOS/nixos-hardware/issues/260
+
+    udev = {
+      extraHwdb = ''
+        evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
+        KEYBOARD_KEY_ff31007c=f20
+      ''; # fixes mic mute button
+    };
+
+    xserver.videoDrivers = [ "modesetting" ]; # TODO determine whether it needs to be specified and what value should be used
+  };
+
+  # Disable power auto-suspend for the ASUS N-KEY device, i.e. USB Keyboard
+  # Disable power wakeup for the 8295 ITE device
+  # Mediatek fine tuning -- not necessary since I'm running AX210
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/SYSTEM"; # assume the Windows boot partition manually created with 512MB
+    fsType = "vfat";
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; } # not sure if 32 or 64GB
+  ];
+
+  # Specialisation for enabling the RTX 4070 GPU
+  specialisation.nvidia.configuration = {
+    hardware.nvidia = {
+      dynamicBoost.enable = true;
+
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      open = true;
+
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+
+        amdgpuBusId = "PCI:69:0:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+
+    services.xserver.videoDrivers = [ "nvidia" ]; # adds to the list of video drivers
+  };
 }
